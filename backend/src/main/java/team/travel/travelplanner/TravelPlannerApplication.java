e47bbdf829b9c7cd90865741c5eec2fb0cb1d26e
Lukas DeLoach <lukasdeloach@gmail.com>
2024-02-03 16:43:02 -0500

    feature: added places nearby method. Now have to observe json data and transform

--- backend/src/main/java/team/travel/travelplanner/TravelPlannerApplication.java
+++ backend/src/main/java/team/travel/travelplanner/TravelPlannerApplication.java
@@ -3,6 +3,7 @@ package team.travel.travelplanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import team.travel.travelplanner.service.impl.GoogleMaps.GoogleMapsApiClient;
+import team.travel.travelplanner.type.LatLng;
 
 @SpringBootApplication
 public class TravelPlannerApplication {
@@ -11,11 +12,11 @@ public class TravelPlannerApplication {
         SpringApplication.run(TravelPlannerApplication.class, args);
         GoogleMapsApiClient client = new GoogleMapsApiClient();
 
-        double departureLat = 39.71899847525047;
-        double departureLng = -75.09783609674565;
-        double arrivalLat = 39.710785873747376;
-        double arrivalLng = -75.119957824938;
-        client.getDirections(departureLat, departureLng, arrivalLat, arrivalLng);
+        LatLng departure = new LatLng(39.71899847525047, -75.09783609674565);
+        LatLng arrival = new LatLng(39.710785873747376, -75.119957824938);
+
+        //client.getDirections(departure, arrival);
+        client.getPlacesNearby(null, arrival, 2000, null);
     }
 
 }
--- backend/src/main/java/team/travel/travelplanner/service/impl/GasStationServiceImpl.java
+++ backend/src/main/java/team/travel/travelplanner/service/impl/GasStationServiceImpl.java
@@ -2,13 +2,18 @@ package team.travel.travelplanner.service.impl;
 
 import team.travel.travelplanner.entity.GasStation;
 import team.travel.travelplanner.service.GasStationService;
+import team.travel.travelplanner.service.impl.GoogleMaps.GoogleMapsApiClient;
+import team.travel.travelplanner.type.LatLng;
 
 import java.util.Collections;
 import java.util.List;
 
 public class GasStationServiceImpl implements GasStationService {
 
-    public List<GasStation> findCheapestGasStation(double departureLat, double departureLng, double arrivalLat, double arrivalLng){
+    private GoogleMapsApiClient apiClient;
+
+    public List<GasStation> findCheapestGasStation(LatLng departure, LatLng arrival){
+
 
         return Collections.emptyList();
     }
