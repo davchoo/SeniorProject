dad32ea806efe61ca13b5d8277ac3e394e886d60
Emma Zimmerman <emma.ar.zimmerman@gmail.com>
2024-02-04 13:37:55 -0500

    added content to About and Contact

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -3,10 +3,19 @@ import React from 'react';
 
 function Contact() {
   return (
-    <div>
-      <h1>Contact Page</h1>
-      {/* Add contact form or other content */}
-    </div>
+    <div className="max-w-screen-md mx-auto p-4">
+      <h2 className="font-notosansjp text-2xl font-bold mb-4">Contact Information</h2>
+      <p className="text-lg leading-relaxed">
+        Thank you for your interest in TripEase! If you have any questions or need assistance, feel free to reach out to us using the contact information below:
+      </p>
+
+      <div className="mt-4">
+        <p className="text-lg font-bold">Emails:</p>
+        <p className="text-lg">emma.ar.zimmerman@gmail.com</p>
+      </div>
+
+      </div>
+
   );
 }
 
