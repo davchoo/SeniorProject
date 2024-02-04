60d46e231048c09429db0e93c7f96af2c3dd1952
Amberichu <4722249+davchoo@users.noreply.github.com>
2024-02-04 13:28:10 -0500

    feature: WIP Route Weather API
    Draft API
    POST /check_route_weather
    {
        polyline: "kgjqFrvmiMnKtl@gi@jc@cY}h@"
        durations: [60, 20, 40],
        startTime: "2024-02-04T17:41:34.494075+00:00"
    }
    polyline is an encoded list of latitude and longitudes
    durations is a list with the time in seconds to traverse each segment of
    the polyline
      durations[1] seconds to travel from points[1] to points[2]
    startTime is the start date and time in ISO 8601 format
      OK [{
            i: 123,
            weatherFeatureType: "RAIN|SNOW|MIXED_PRECIPITATION|..."
            forecastDay: 1|2|3
          }, ...]
          i correspond to the segment formed by points[i] and points[i+1]
          weatherFeatureType denotes the forecasted weather for this segment
          forecastDay ???
    
    TODO: Only return the latest forecasts
    TODO: clean it up a bit

---
+++ backend/src/main/java/team/travel/travelplanner/model/RouteWeatherFeature.java
@@ -0,0 +1,4 @@
+package team.travel.travelplanner.model;
+
+public record RouteWeatherFeature(int i, String weatherFeatureType, int forecastDay) {
+}
--- backend/src/main/java/team/travel/travelplanner/repository/WeatherForecastFeatureRepository.java
+++ backend/src/main/java/team/travel/travelplanner/repository/WeatherForecastFeatureRepository.java
@@ -1,12 +1,16 @@
 package team.travel.travelplanner.repository;
 
+import org.locationtech.jts.geom.Geometry;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.stereotype.Repository;
 import team.travel.travelplanner.entity.WeatherForecastFeature;
+import team.travel.travelplanner.model.RouteWeatherFeature;
 
+import java.time.Instant;
 import java.time.ZonedDateTime;
+import java.util.List;
 
 @Repository
 public interface WeatherForecastFeatureRepository extends JpaRepository<WeatherForecastFeature, Long> {
@@ -15,4 +19,7 @@ public interface WeatherForecastFeatureRepository extends JpaRepository<WeatherF
 
     @Procedure("deduplicate_weather_forecast_features")
     void deduplicate();
+
+    @Query(nativeQuery = true)
+    List<RouteWeatherFeature> checkRouteWeather(Geometry route, int[] durations, Instant startTime);
 }
