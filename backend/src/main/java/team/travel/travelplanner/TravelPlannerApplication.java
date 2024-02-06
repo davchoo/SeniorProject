aa9e99ce99d460017e99df902c5194668defd58e
Lukas DeLoach <lukasdeloach@gmail.com>
2024-02-05 21:16:14 -0500

    feature: added findPlaces method that queries based on location, type, and radius.

--- backend/src/main/java/team/travel/travelplanner/TravelPlannerApplication.java
+++ backend/src/main/java/team/travel/travelplanner/TravelPlannerApplication.java
@@ -4,7 +4,8 @@ import com.google.maps.errors.ApiException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import team.travel.travelplanner.service.impl.GoogleMaps.GoogleMapsApiClientService;
+import team.travel.travelplanner.service.impl.GasStationServiceImpl;
+import team.travel.travelplanner.type.LatLng;
 
 import java.io.IOException;
 
@@ -12,9 +13,11 @@ import java.io.IOException;
 public class TravelPlannerApplication {
 
     public static void main(String[] args) throws IOException, InterruptedException, ApiException {
+        LatLng departure = new LatLng(39.71899847525047, -75.09783609674565);
+        LatLng arrival = new LatLng(39.710785873747376, -75.119957824938);
         ApplicationContext context = SpringApplication.run(TravelPlannerApplication.class, args);
 
-        GoogleMapsApiClientService client = context.getBean(GoogleMapsApiClientService.class);
-        client.test();
+        GasStationServiceImpl gasStationService = context.getBean(GasStationServiceImpl.class);
+        gasStationService.findCheapestGasStation(departure, arrival);
     }
 }
--- backend/src/main/java/team/travel/travelplanner/type/LatLng.java
+++ backend/src/main/java/team/travel/travelplanner/type/LatLng.java
@@ -1,4 +1,8 @@
 package team.travel.travelplanner.type;
 
 public record LatLng(double latitude, double longitude) {
+
+    public String toString(){
+        return latitude + "," +longitude;
+    }
 }
