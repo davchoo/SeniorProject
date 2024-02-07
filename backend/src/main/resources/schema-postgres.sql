0bdd877f034e926ab66f96f8f05d2c1a60027f5e
Amberichu <4722249+davchoo@users.noreply.github.com>
2024-02-07 00:42:02 -0500

    fix: Use the latest* available weather forecast when checking route
    TODO: Prefer afternoon issuance when available for segments is in the
    afternoon
    
    Execute updateNow in a transaction to prevent partial updates
    Made sure to flush and clear for
    WeatherForecastFeatureRepository.deduplicate
    
    Fix inconsistent capitalization in schema-postgres.sql

--- backend/src/main/resources/schema-postgres.sql
+++ backend/src/main/resources/schema-postgres.sql
@@ -4,40 +4,49 @@ CREATE OR REPLACE PROCEDURE deduplicate_weather_forecast_features()
 AS
 $$
 DELETE
-FROM weather_forecast_feature as wxff
+FROM weather_forecast_feature AS wxff
 WHERE wxff.id IN
       (SELECT id
        FROM (SELECT *,
                     row_number()
-                    OVER (PARTITION BY wxff.forecast_day, wxff.file_date, wxff.weather_feature_type, wxff.geometry ORDER BY retrieval_timestamp) as row_number
-             FROM weather_forecast_feature as wxff) s
+                    OVER (PARTITION BY wxff.forecast_day, wxff.file_date, wxff.weather_feature_type, wxff.geometry ORDER BY retrieval_timestamp) AS row_number
+             FROM weather_forecast_feature AS wxff) s
        WHERE row_number >= 2)
 $$;
-
+DROP FUNCTION IF EXISTS check_route_weather;
 CREATE OR REPLACE FUNCTION check_route_weather(IN route geometry, IN durations int[],
                                                IN start_time timestamp with time zone)
     RETURNS TABLE
             (
-                i                    int,
-                weather_feature_type varchar(255),
-                forecast_day         int
+                i                           int,
+                weather_feature_type        varchar(255),
+                forecast_day                int
             )
     LANGUAGE sql
 AS
 $$
-WITH route as (SELECT durations.i,
+WITH route AS (SELECT durations.i,
                       start_time +
-                      make_interval(secs := (sum(duration) over (order by durations.i) - duration)) as start_timestamp,
+                      make_interval(secs := (sum(duration) OVER (ORDER BY durations.i) - duration)) AS start_timestamp,
                       start_time +
-                      make_interval(secs := (sum(duration) over (order by durations.i)))            as end_timestamp,
-                      route_segements.g                                                             as segment
-               from unnest(durations) with ordinality as durations(duration, i)
-                        join (select path[1] as i, geom as g
-                              from st_dumpsegments(route)) as route_segements
-                             on durations.i = route_segements.i)
-select route.i, wxff.weather_feature_type, wxff.forecast_day
-from route
-         inner join weather_forecast_feature as wxff
-                    on route.start_timestamp < wxff.valid_end and route.end_timestamp > wxff.valid_start and
-                       st_intersects(route.segment, wxff.geometry)
+                      make_interval(secs := (sum(duration) OVER (ORDER BY durations.i)))            AS end_timestamp,
+                      route_segements.g                                                             AS segment
+               FROM unnest(durations) WITH ORDINALITY AS durations(duration, i)
+                        JOIN (SELECT path[1] AS i, geom AS g
+                              FROM st_dumpsegments(route)) AS route_segements
+                             ON durations.i = route_segements.i),
+     s(latest_valid_end) AS (SELECT max(wxff.valid_end) FROM weather_forecast_feature AS wxff WHERE forecast_day = 1)
+SELECT route.i - 1,
+       wxff.weather_feature_type,
+       wxff.forecast_day
+FROM route
+         JOIN s ON TRUE
+         INNER JOIN weather_forecast_feature AS wxff
+                    ON
+                        (
+                            forecast_day = greatest(floor(date_part('epoch', route.start_timestamp - s.latest_valid_end) / 86400) + 2, 1) OR
+                            forecast_day = greatest(floor(date_part('epoch', route.end_timestamp - s.latest_valid_end) / 86400) + 2, 1)
+                        ) AND
+                        (route.start_timestamp, route.end_timestamp) OVERLAPS (wxff.valid_start, wxff.valid_end) AND
+                        st_intersects(route.segment, wxff.geometry)
 $$;
