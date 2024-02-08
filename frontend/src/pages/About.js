ebb67479adf281533541fba7ade0e1513ca14559
Emma Zimmerman <emma.ar.zimmerman@gmail.com>
2024-02-08 12:57:40 -0500

    udate about and contact

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -3,20 +3,23 @@ import React from 'react';
 
 function About() {
   return (
-    <div className="max-w-screen-md mx-auto p-4">
-      <h2 className="font-notosansjp"> About TripEase</h2>
+    <div className="flex justify-center items-center h-full">
+      <div className="max-w-screen-md mx-auto p-8 bg-custom-green m-5"> 
+        <h2 className="font-notosansjp font-bold text-center mb-12"> About TripEase</h2>
+        <div className="max-w-md w-full px-8 py-6 bg-white rounded-lg shadow-md">
+          <p className="text-lg leading-relaxed">
+            TripEase is a web application tailored for individuals at all levels of travel experience to streamline the process of planning any trip. The anticipated sign-up process will guide users through the journey planning process, simplifying tasks from selecting departure and destination points to finalizing itineraries within an intuitive web interface. This web application will adapt its interface to fit various screen sizes on mobile devices to ensure flexibility for users on the go.
+          </p>
 
-        <p className="text-lg leading-relaxed">
-          TripEase is a web application tailored for individuals at all levels of travel experience to streamline the process of planning any trip. The anticipated sign-up process will guide users through the journey planning process, simplifying tasks from selecting departure and destination points to finalizing itineraries within an intuitive web interface. This web application will adapt its interface to fit various screen sizes on mobile devices to ensure flexibility for users on the go.
-        </p>
+          <p className="text-lg leading-relaxed">
+            TripEase will provide users with an effortless means to discover rental cars within their specified budget by integrating with car rental APIs. Users can filter cars based on price, vehicle type, and features, ensuring a customized selection for a personalized road trip adventure. Additionally, TripEase will show flights to their destinations, empowering users to make informed decisions without sacrificing convenience. For budget-conscious travelers, TripEase will locate convenient and cost-effective gas stops along their route. The application will inform users about unpredictable weather conditions through real-time weather updates.
+          </p>
 
-        <p className="text-lg leading-relaxed">
-          TripEase will provide users with an effortless means to discover rental cars within their specified budget by integrating with car rental APIs. Users can filter cars based on price, vehicle type, and features, ensuring a customized selection for a personalized road trip adventure. Additionally, TripEase will show flights to their destinations, empowering users to make informed decisions without sacrificing convenience. For budget-conscious travelers, TripEase will locate convenient and cost-effective gas stops along their route. The application will inform users about unpredictable weather conditions through real-time weather updates.
-        </p>
-
-        <p className="text-lg leading-relaxed">
-          TripEase's integration with various APIs guarantees a seamless flow of current information, encompassing possible details such as weather updates, flight options, gas prices, and car rentals. This comprehensive integration will enhance the efficiency of the web application, ensuring users have accurate and up-to-date insights for a more streamlined trip-planning experience.
-        </p>
+          <p className="text-lg leading-relaxed">
+            TripEase's integration with various APIs guarantees a seamless flow of current information, encompassing possible details such as weather updates, flight options, gas prices, and car rentals. This comprehensive integration will enhance the efficiency of the web application, ensuring users have accurate and up-to-date insights for a more streamlined trip-planning experience.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ frontend/src/pages/Contact.js
@@ -0,0 +1,29 @@
+// components/Contact.js
+import React from 'react';
+
+function Contact() {
+  return (
+    <div className="flex justify-center items-center h-full">
+      <div className="max-w-screen-md mx-auto p-8 bg-custom-green m-5"> {/* Increased maximum width */}
+        <h2 className="font-notosansjp text-2xl text-center font-bold mb-12">Contact Information</h2>
+        <div className="max-w-md w-full px-8 py-6 bg-white rounded-lg shadow-md">
+          <p className="text-lg leading-relaxed text-gray-700"> {/* Changed text color */}
+            Thank you for your interest in TripEase! If you have any questions or need assistance, feel free to reach out to us using the contact information below:
+          </p>
+
+          <div className="mt-4">
+            <ul className="list-disc">
+              <li className="text-lg"> Emma Zimmerman (<a href="https://github.com/EmmaZim">GitHub</a>, <a href="mailto:emma.ar.zimmerman@gmail.com">emma.ar.zimmerman@gmail.com</a>)</li>
+              <li className="text-lg"> David Choo (<a href="https://github.com/davchoo">GitHub</a>)</li>
+              <li className="text-lg"> Lukas DeLoach (<a href="https://github.com/lukasdeloach">GitHub</a>)</li>
+              <li className="text-lg"> Kaan Kayis (<a href="https://github.com/kaankayis22">GitHub</a>)</li>
+              <li className="text-lg"> Yekaterina Saburova (<a href="https://github.com/KatSaburova">GitHub</a>, <a href="mailto:yeksbr@gmail.com">yeksbr@gmail.com</a>)</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
