d93d964a8b2cf2cb783195f1fd364062b5aa64ef
Lukas DeLoach <lukasdeloach@gmail.com>
2024-02-11 12:18:55 -0500

    feature: added methods to find gas stations along a route and calculate the station with the lowest prices at each stop. These stops are estimated by findNeededStops.
    Not thouroughly tested! Every search is very expensive due to the amount of API calls to be made. Will figure out best options to go about testing code.

--- backend/src/main/java/team/travel/travelplanner/service/GoogleMapsApiClientService.java
+++ backend/src/main/java/team/travel/travelplanner/service/GoogleMapsApiClientService.java
@@ -1,17 +1,5 @@
 package team.travel.travelplanner.service;
 
-import com.google.maps.errors.ApiException;
-import com.google.maps.model.DirectionsResult;
-import com.google.maps.model.LatLng;
-import team.travel.travelplanner.model.FuelOptions;
-
-import java.io.IOException;
-
 public interface GoogleMapsApiClientService {
-
-
-    void findPlaces(LatLng location, String type, int radius);
-
-    void getPlaceDetails(String place);
 
 }
