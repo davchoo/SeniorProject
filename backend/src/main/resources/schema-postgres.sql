b08ed577b4a0a3f574c303c40c974518f5122237
Amberichu <4722249+davchoo@users.noreply.github.com>
2024-02-12 13:34:22 -0500

    refactor: Use ms for duration, missing check, and add comments for query
    milliseconds is the unit used by GraphHopper.

--- backend/src/main/resources/schema-postgres.sql
+++ backend/src/main/resources/schema-postgres.sql
@@ -30,9 +30,9 @@ AS
 $$
 WITH route AS (SELECT durations.i,
                       start_time +
-                      make_interval(secs := (sum(duration) OVER (ORDER BY durations.i) - duration)) AS start_timestamp,
+                      make_interval(secs := (sum(duration) OVER (ORDER BY durations.i) - duration) / 1000.0) AS start_timestamp,
                       start_time +
-                      make_interval(secs := (sum(duration) OVER (ORDER BY durations.i)))            AS end_timestamp,
+                      make_interval(secs := (sum(duration) OVER (ORDER BY durations.i)) / 1000.0)            AS end_timestamp,
                       route_segements.g                                                             AS segment
                FROM unnest(durations) WITH ORDINALITY AS durations(duration, i)
                         JOIN (SELECT path[1] AS i, geom AS g
@@ -53,19 +53,20 @@ FROM route
          JOIN s ON TRUE
          INNER JOIN weather_forecast_feature AS wxff
                     ON
-                        (route.start_timestamp, route.end_timestamp) OVERLAPS (wxff.valid_start, wxff.valid_end) AND
+                        (route.start_timestamp, route.end_timestamp) OVERLAPS (wxff.valid_start, wxff.valid_end) AND -- Segment overlaps with valid period for issuance
                         (
-                            forecast_day = greatest(floor(date_part('epoch', route.start_timestamp - s.latest_valid_end) / 86400) + 2, 1) OR
+                            forecast_day = greatest(floor(date_part('epoch', route.start_timestamp - s.latest_valid_end) / 86400) + 2, 1) OR -- Match with latest issuance for a time range
                             forecast_day = greatest(floor(date_part('epoch', route.end_timestamp - s.latest_valid_end) / 86400) + 2, 1)
                         ) AND
                         (
-                            (forecast_day != 1 AND (s.latest_file_date - wxff.file_date) < interval '1 hour') OR
+                            (forecast_day != 1 AND (s.latest_file_date - wxff.file_date) < interval '1 hour') OR -- Segment is in the future, match with latest stored issuance
                             (forecast_day = 1 AND (
-                                route.end_timestamp - route.start_timestamp > interval '12 hour' OR
-                                date_part('hour', route.start_timestamp AT TIME ZONE 'EST') < 19 OR
-                                date_part('hour', wxff.file_date AT TIME ZONE 'EST') >= 12 OR
-                                ((s.latest_file_date - wxff.file_date) < interval '1 hour' AND date_part('hour', s.latest_valid_start AT TIME ZONE 'EST') = 7)
+                                route.end_timestamp - route.start_timestamp > interval '12 hour' OR -- Duration is greater than 12 hours, match with morning issuance
+                                date_part('hour', route.start_timestamp AT TIME ZONE 'EST') < 19 OR -- Duration starts before 7 pm, match with morning issuance
+                                date_part('hour', route.end_timestamp AT TIME ZONE 'EST') > 7 OR    -- Duration ends after 7 am, match with morning issuance
+                                date_part('hour', wxff.file_date AT TIME ZONE 'EST') >= 12 OR       -- Always match with afternoon issuance
+                                ((s.latest_file_date - wxff.file_date) < interval '1 hour' AND date_part('hour', s.latest_valid_start AT TIME ZONE 'EST') = 7) -- Missing latest afternoon issuance, match with morning issuance
                             ))
                         ) AND
-                        st_intersects(route.segment, wxff.geometry)
+                        st_intersects(route.segment, wxff.geometry) -- Segment intersects the weather feature
 $$;
