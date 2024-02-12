68a55d03a05ab06a85d7e565d144199d6a9a7584
Amberichu <4722249+davchoo@users.noreply.github.com>
2024-02-12 13:39:59 -0500

    refactor: Move logic out of WeatherController
    Converting the encoded polyline to a line string is handled by
    Route#geometry(GeometryFactory)
    Added checkRouterWeather to WeatherDataService that calls repository
    method.
      - Keep implementation details out of controllers?
    Changed RouteWeatherFeature.weatherFeatureType to use the enum for later
    unit tests.
    Changed endpoint to /api/weather/check_route and renamed controller

---
+++ backend/src/main/java/team/travel/travelplanner/controller/WeatherController.java
@@ -0,0 +1,33 @@
+package team.travel.travelplanner.controller;
+
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.PrecisionModel;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import team.travel.travelplanner.model.Route;
+import team.travel.travelplanner.model.RouteWeatherFeature;
+import team.travel.travelplanner.service.WeatherDataService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/weather")
+public class WeatherController {
+    private final WeatherDataService weatherDataService;
+
+    private final GeometryFactory geometryFactory;
+
+    private static final int WGS84_SRID = 4326;
+
+    public WeatherController(WeatherDataService weatherDataService) {
+        this.weatherDataService = weatherDataService;
+        this.geometryFactory = new GeometryFactory(new PrecisionModel(), WGS84_SRID);
+    }
+
+    @PostMapping("/check_route")
+    public List<RouteWeatherFeature> checkRoute(@RequestBody Route route) {
+        return weatherDataService.checkRouteWeather(route.geometry(geometryFactory), route.durations(), route.startTime());
+    }
+}
