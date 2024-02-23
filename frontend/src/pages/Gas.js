d3a299dcfdcbf0acd70cd2316832c105a4f47ee7
Emma Zimmerman <emma.ar.zimmerman@gmail.com>
2024-02-22 22:58:57 -0500

    added buttons

---
+++ frontend/src/pages/Gas.js
@@ -0,0 +1,14 @@
+const Gas = () => {
+    return (
+      <div style={{ marginBottom: '20px', marginTop: '20px', display: 'flex', flexDirection: 'column', alignItems: 'flex-start'}}>
+        <button
+            type="submit"
+            className="w-full mt-5 py-2 px-4 bg-custom-green3 text-custom-black font-notosansjp font-bold rounded-lg shadow-md hover:bg-custom-green hover:text-white focus:outline-none focus:bg-custom-green focus:text-white"
+          >
+            Show Gas Stations        
+              </button>
+      </div>
+    );
+  };
+
+  export default Gas;  
---
+++ frontend/src/pages/Weather.js
@@ -0,0 +1,9 @@
+const Weather = () => {
+    return (
+      <div style={{ marginBottom: '20px', marginTop: '20px', display: 'flex', flexDirection: 'column', alignItems: 'flex-start'}}>
+       
+      </div>
+    );
+  };
+
+  export default Weather;  
