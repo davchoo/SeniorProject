33241d9ad5476f1b15db427eb5bd4b74df8058de
Emma Zimmerman <emma.ar.zimmerman@gmail.com>
2024-03-05 17:30:55 -0500

    updated weather setup

--- frontend/src/pages/Weather.js
+++ frontend/src/pages/Weather.js
@@ -1,9 +1,57 @@
+import React, { useState } from 'react';
+
 const Weather = () => {
-    return (
-      <div style={{ marginBottom: '20px', marginTop: '20px', display: 'flex', flexDirection: 'column', alignItems: 'flex-start'}}>
-       
-      </div>
-    );
+  const [selectedOption, setSelectedOption] = useState(null);
+  const [showLegend, setShowLegend] = useState(false);
+
+  const handleOptionChange = (value) => {
+    setSelectedOption(value);
   };
 
-  export default Weather;  
+  const toggleLegend = () => {
+    setShowLegend(!showLegend);
+  };
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', marginTop: '20px' }}>
+      <label>
+        <input
+          type="radio"
+          name="weatherOption"
+          value="overlay"
+          checked={selectedOption === 'overlay'}
+          onChange={() => handleOptionChange('overlay')}
+        />
+        Show Weather Overlay
+      </label>
+
+      <label style={{ marginTop: '10px' }}>
+        <input
+          type="radio"
+          name="weatherOption"
+          value="radar"
+          checked={selectedOption === 'radar'}
+          onChange={() => handleOptionChange('radar')}
+        />
+        Show Weather Radar
+      </label>
+
+      <div style={{ marginTop: '250px', marginLeft: 'auto', marginRight: '-400px' }}>
+        <button
+          className={`font-notosansjp font-extrabold text-custom-black ${showLegend ? 'bg-custom-green4' : 'bg-custom-green3'} py-1 px-2 rounded-md mb-2`}
+          onClick={toggleLegend}
+        >
+          {showLegend ? 'Hide' : 'Show'} Legend
+        </button>
+
+        {showLegend && (
+          <div style={{ marginTop: '10px', marginRight: '-400px'}}>
+            <p style={{ color: '#333' }}>Legend Content Here</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Weather;
