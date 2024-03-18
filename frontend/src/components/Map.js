2ae097118a87686df516b63ec338e2a7689c6140
Emma Zimmerman <emma.ar.zimmerman@gmail.com>
2024-03-18 19:28:19 -0400

    Updates for the invalid location check

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import { GoogleMap, useLoadScript, Marker, DirectionsRender, Polyline } from '@react-google-maps/api';
+import React, { useState } from 'react';
+import { GoogleMap, useLoadScript, Marker, Polyline, InfoWindow } from '@react-google-maps/api';
 import { AutoComplete } from './AutoComplete';
 import axios from "axios";
 
 const libraries = ['places'];
 const mapContainerStyle = {
   width: '65vw',
-  height: '90vh',
+  height: '70vh',
   border: '4px solid white',
   borderRadius: '8px',
-  marginLeft: 'auto', 
+  marginLeft: 'auto',
 };
 
 const center = {
@@ -17,79 +17,86 @@ const center = {
   lng: -98.5795, // Longitude of the center of the USA
 };
 
-
 const Map = () => {
   const { isLoaded, loadError } = useLoadScript({
     
+googleMapsApiKey: 'AIzaSyAquT4y7VbS6RKyYn6Nbpjrm5Wcng-O-oo',
 
     libraries,
   });
 
-  const [origin, setOrigin] = useState(null)
-  const [destination, setDestination] = useState(null)
+  const [origin, setOrigin] = useState(null);
+  const [destination, setDestination] = useState(null);
   const [directions, setDirections] = useState(null);
-  const [gasStations, setGasStations] = useState([])
+  const [gasStations, setGasStations] = useState([]);
+  const [infoWindow, setInfoWindow] = useState(null);
 
   const handlePlaceSelect = (selectedPlace, isOrigin) => {
-    if (selectedPlace && selectedPlace.geometry && selectedPlace.geometry.location) {
-      const lat = selectedPlace.geometry.location.lat();
-      const lng = selectedPlace.geometry.location.lng();
-      if (isOrigin) {
-        setOrigin({ lat, lng });
-      } else {
-        setDestination({ lat, lng });
-      }
+  if (selectedPlace && selectedPlace.geometry && selectedPlace.geometry.location) {
+    const lat = selectedPlace.geometry.location.lat();
+    const lng = selectedPlace.geometry.location.lng();
+    if (isOrigin) {
+      setOrigin({ lat, lng });
     } else {
-      console.error('Invalid place object:', selectedPlace);
+      setDestination({ lat, lng });
     }
-  };
-
-  const getGasStations = () => {
-    if(origin != null && destination != null){
-
-    // Make an HTTP POST request to your backend
-    axios.post('http://localhost:8080/api/trip/gas', {
-        originLat: origin.lat,
-        originLng: origin.lng,
-        destinationLat: destination.lat,
-        destinationLng: destination.lng,
-        type: "REGULAR_UNLEADED", // need to get this from what Kaan is working on
-        travelersMeterCapacity: 482803 // need to get this from what Kaan is working on
-    })
-        .then(response => {
-          // Handle success
-          setDirections(response.data.directionsResult)
-          setGasStations(response.data.gasStationList)
-          console.log("Response from backend:", response.data);
-      
-        })
-        .catch(error => {
-          // Handle error
-          console.error("Error getting gas stations:", error);
-        });
+  } else {
+    console.error('Invalid place object:', selectedPlace);
+    if (isOrigin) {
+      setOrigin(null); 
+    } else {
+      setDestination(null); 
     }
-  };
+  }
+};
 
-  // useEffect(() => {
-  //   if (origin && destination) {
-  //     const directionsService = new window.google.maps.DirectionsService();
-  //     directionsService.route(
-  //       {
-  //         origin: new window.google.maps.LatLng(origin.lat, origin.lng),
-  //         destination: new window.google.maps.LatLng(destination.lat, destination.lng),
-  //         travelMode: 'DRIVING',
-  //       },
-  //       (result, status) => {
-  //         if (status === 'OK') {
-  //           console.log(result)
-  //           setDirections(result);
-  //         } else {
-  //           console.error('Failed to fetch directions. Status: ', status);
-  //         }
-  //       }
-  //     );
-  //   }
-  // }, [origin, destination]);
+const getGasStations = () => {
+  if(origin != null && destination != null){
+
+  // Make an HTTP POST request to your backend
+  axios.post('http://localhost:8080/api/trip/gas', {
+      originLat: origin.lat,
+      originLng: origin.lng,
+      destinationLat: destination.lat,
+      destinationLng: destination.lng,
+      type: "REGULAR_UNLEADED", // need to get this from what Kaan is working on
+      travelersMeterCapacity: 482803 // need to get this from what Kaan is working on
+  })
+      .then(response => {
+        // Handle success
+        setDirections(response.data.directionsResult)
+        setGasStations(response.data.gasStationList)
+        console.log("Response from backend:", response.data);
+    
+      })
+      .catch(error => {
+        // Handle error
+        console.error("Error getting gas stations:", error);
+      });
+  }
+};
+
+// useEffect(() => {
+//   if (origin && destination) {
+//     const directionsService = new window.google.maps.DirectionsService();
+//     directionsService.route(
+//       {
+//         origin: new window.google.maps.LatLng(origin.lat, origin.lng),
+//         destination: new window.google.maps.LatLng(destination.lat, destination.lng),
+//         travelMode: 'DRIVING',
+//       },
+//       (result, status) => {
+//         if (status === 'OK') {
+//           console.log(result)
+//           setDirections(result);
+//         } else {
+//           console.error('Failed to fetch directions. Status: ', status);
+//         }
+//       }
+//     );
+//   }
+// }, [origin, destination]);
+
 
   if (loadError) {
     return <div>Error Loading Maps</div>;
@@ -101,74 +108,108 @@ const Map = () => {
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'flex-start', height: '100vh', marginTop: '20px' }}>
-      <div style={{ width: '30vw', marginBottom: '20px', display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+      <div style={{ width: '30vw', marginBottom: '20px', marginTop: '80px', display: 'flex', flexDirection: 'column', alignItems: 'flex-start', zIndex: 1 }}>
         <AutoComplete handlePlaceSelect={(place) => handlePlaceSelect(place, true)} label="Enter Origin:" />
         <AutoComplete handlePlaceSelect={(place) => handlePlaceSelect(place, false)} label="Enter Destination:" />
+
       </div>
-      <button onClick={getGasStations}>Gas Stations</button>
       <GoogleMap
         mapContainerStyle={mapContainerStyle}
         zoom={4.6}
-        center={ center}
+        center={center}
       >
-         {origin != null ? <Marker
-          position={{
-            lat: origin.lat,
-            lng: origin.lng
-          }}
-          label={{
-            text: 'A',
-            color: 'black', 
-            fontSize: '17px', 
-            fontWeight: 'bold',
-            
-          }}
-        /> : null}
-        {destination != null ? <Marker
+        {origin != null ? (
+          <Marker
+            position={{
+              lat: origin.lat,
+              lng: origin.lng,
+            }}
+            label={{
+              text: 'A',
+              color: 'black',
+              fontSize: '17px',
+              fontWeight: 'bold',
+            }}
+            onClick={() => setInfoWindow({ type: 'origin', location: origin })}
+          />
+        ) : null}
+        {infoWindow && infoWindow.type === 'origin' && (
+          <InfoWindow
+            position={{
+              lat: infoWindow.location.lat,
+              lng: infoWindow.location.lng,
+            }}
+            onCloseClick={() => setInfoWindow(null)}
+          >
+            <div style={{ padding: '5px', maxWidth: '150px' }}>
+              <p style={{ margin: '0' }}>Origin Location</p>
+              <p style={{ margin: '0', fontSize: '12px' }}></p>
+            </div>
+          </InfoWindow>
+        )}
+
+        {destination != null ? (
+          <Marker
             position={{
               lat: destination.lat,
-              lng: destination.lng
+              lng: destination.lng,
             }}
             label={{
               text: 'B',
               color: 'black',
               fontSize: '17px',
-              fontWeight: 'bold'
+              fontWeight: 'bold',
             }}
-          />: null}
+            onClick={() => setInfoWindow({ type: 'destination', location: destination })}
+          />
+        ) : null}
+        {infoWindow && infoWindow.type === 'destination' && (
+          <InfoWindow
+            position={{
+              lat: infoWindow.location.lat,
+              lng: infoWindow.location.lng,
+            }}
+            onCloseClick={() => setInfoWindow(null)}
+          >
+            <div style={{ padding: '5px', maxWidth: '150px' }}>
+              <p style={{ margin: '0' }}>Destination Location</p>
+              <p style={{ margin: '0', fontSize: '12px' }}></p>
+            </div>
+          </InfoWindow>
+        )}
 
         {directions && directions.routes && directions.routes.length > 0 && (
-                  <>
-                    <Polyline
-                        path={decodePolyline(directions.routes[0].overviewPolyline.encodedPath)}
-                        options={{
-                            strokeColor: '#FF0000',
-                            strokeOpacity: 1.0,
-                            strokeWeight: 3
-                        }}
-                    />
-                    </>
-                )}
-      {gasStations != null ? 
-      gasStations.map(station => (
-        <Marker
-            key={station.name}
-            position={{
+          <>
+            <Polyline
+              path={decodePolyline(directions.routes[0].overviewPolyline.encodedPath)}
+              options={{
+                strokeColor: '#FF0000',
+                strokeOpacity: 1.0,
+                strokeWeight: 3
+              }}
+            />
+          </>
+        )}
+
+        {gasStations != null ?
+          gasStations.map(station => (
+            <Marker
+              key={station.name}
+              position={{
                 lat: station.location.latitude,
                 lng: station.location.longitude
-            }}
-            title={station.formattedAddress}
-            icon={{
-              url: 'https://maps.google.com/mapfiles/ms/icons/green-dot.png', // Change this URL to the desired marker color
-              scaledSize: new window.google.maps.Size(30, 30) // Adjust the size of the marker icon if necessary
-          }}
-        />
-    ))
-    : null}
+              }}
+              title={station.formattedAddress}
+              icon={{
+                url: 'https://maps.google.com/mapfiles/ms/icons/green-dot.png',
+                scaledSize: new window.google.maps.Size(30, 30)
+              }}
+            />
+          ))
+          : null}
       </GoogleMap>
     </div>
   );
-
 };
 
 function decodePolyline(encoded) {
