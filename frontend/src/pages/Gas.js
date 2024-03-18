a6aee4fa44fccd69487f864eac3cfe5e32288b51
EmmaZim <125603041+EmmaZim@users.noreply.github.com>
2024-03-18 00:54:59 -0400

    Update Gas.js

--- frontend/src/pages/Gas.js
+++ frontend/src/pages/Gas.js
@@ -1,22 +1,66 @@
-// Gas.js
-import React from 'react';
+import React, { useState } from 'react';
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 
-const Gas = ({ showGasInfo, onToggleGasInfo }) => {
+const Gas = ({ showGasInfo }) => {
+  const [selectedMake, setSelectedMake] = useState('');
+  const [selectedModel, setSelectedModel] = useState('');
+  const [showGasStations, setShowGasStations] = useState(false);
+
+  const toggleGasStations = () => {
+    setShowGasStations(!showGasStations);
+  };
+
+  const GasStationsMarkers = ({ gasStations }) => (
+    <>
+      {gasStations.map((station) => (
+        <Marker
+          key={station.name}
+          position={{
+            lat: station.location.latitude,
+            lng: station.location.longitude,
+          }}
+          title={station.formattedAddress}
+          icon={{
+            url: 'https://maps.google.com/mapfiles/ms/icons/green-dot.png',
+            scaledSize: new window.google.maps.Size(30, 30),
+          }}
+        />
+      ))}
+    </>
+  );
+
   return (
-    <div style={{ marginBottom: '20px', marginTop: '20px', display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
-      <button
-        type="button"
-        onClick={onToggleGasInfo}
-        className="w-full mt-5 py-0 px-1 bg-custom-green3 text-custom-black font-notosansjp font-bold rounded-lg shadow-md hover:bg-custom-green hover:text-white focus:outline-none focus:bg-custom-green focus:text-white"
-      >
-        Show Gas Stations
-      </button>
-      {showGasInfo && (
+    <div>
+      <div style={{ position: 'absolute', right: '12px', top: '130px' }}>
+        <p className="text-sm text-custom-black font-notosansjp">Enter Information About Your Vehicle:</p>
+      </div>
+
+      <div style={{ display: 'flex', justifyContent: 'space-between', padding: '10px', marginTop: '50px'}}>
         <div>
-          {/* This is where we can try and add some of the gas station information */}
-          Gas Station Information
+          <p className="text-sm text-custom-black font-notosansjp">Car Make:</p>
+          <select value={selectedMake} onChange={(e) => setSelectedMake(e.target.value)}>
+            <option value="">Select Make</option>
+          </select>
         </div>
-      )}
+
+        <div>
+          <div style={{ marginLeft: '30px' }}> 
+            <p className="text-sm text-custom-black font-notosansjp">Car Model:</p>
+            <select value={selectedModel} onChange={(e) => setSelectedModel(e.target.value)}>
+              <option value="">Select Model</option>
+            </select>
+          </div>
+        </div>
+      </div>
+
+      <div style={{ padding: '10px' }}>
+        <button
+          onClick={toggleGasStations}
+          className={`font-notosansjp font-extrabold mr-10 mt-10 text-custom-black ${showGasStations ? 'bg-custom-green4' : 'bg-custom-green3'} py-1 px-2 rounded-md mb-2`}
+        >
+          Show Gas Stations
+        </button>
+      </div>
     </div>
   );
 };
