e37f65da722f68709d30ce637936380310012f16
Emma Zimmerman <emma.ar.zimmerman@gmail.com>
2024-03-21 12:57:51 -0400

    adding to the Gas page

--- frontend/src/pages/Plan.js
+++ frontend/src/pages/Plan.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
 import Map from '../components/Map';
 import Sidebar from '../components/SaveSidebar';
-import { GiHamburgerMenu } from 'react-icons/gi';
 import Gas from '../pages/Gas';
 import Weather from '../pages/Weather';
 
@@ -16,12 +16,12 @@ function Plan() {
 
   const toggleGasInfo = () => {
     setShowGasInfo(!showGasInfo);
-    setShowWeatherInfo(false); // This should close the Weather button when Gas is opened
+    setShowWeatherInfo(false); // Close the Weather info when Gas Stations are opened
   };
 
   const toggleWeatherInfo = () => {
     setShowWeatherInfo(!showWeatherInfo);
-    setShowGasInfo(false); // This should close the Gas button when Weather is opened
+    setShowGasInfo(false); // Close the Gas Stations info when Weather is opened
   };
 
   const closeSidebar = () => {
@@ -29,11 +29,12 @@ function Plan() {
   };
 
   return (
-    <div className="min-h-screen bg-custom-green flex justify-center items-center font-notosansjp font-bold m-1">
-      <div className="w-full max-w-screen-xl relative">
-        <button onClick={toggleSidebar} className="fixed top-4 left-4 text-xl text-white">
-          <GiHamburgerMenu />
-        </button>
+    <div className="min-h-screen bg-custom-green flex justify-center items-center font-notosansjp font-bold m-1 relative z-0">
+      <div className="w-full max-w-screen-xl relative h-full"> 
+      <button onClick={toggleSidebar} className="fixed top-32 left-4 text-xl text-white">
+        <GiHamburgerMenu />
+      </button>
+
 
         <Sidebar isOpen={isSidebarOpen} onClose={closeSidebar}>
           <div style={{ padding: '20px', borderRight: '2px solid white' }}>
@@ -42,35 +43,40 @@ function Plan() {
         </Sidebar>
 
         <div className="flex flex-col justify-center items-center m-2">
-          <p className="mt-4 text-center text-2xl text-custom-black">
-            Let's Start Planning!
+          <p className="mt-4 text-center text-3xl text-custom-black">
+            Let's Start Planning Your Trip!
           </p>
 
-          <div className="max-w-md w-full px-0 py-0 mb-3 bg-white rounded-lg shadow-md">
-            <div className="text-center text-sm text-custom-black mt-2">
-              <p>Choose your departure and destination points below:</p>
+          <div style={{ position: 'absolute', left: '-105px', bottom: '-10px', top: '140px' }}>
+            <p className="text-sm text-custom-black font-notosansjp">
+              Provide your origin and destination locations to begin.
+            </p>
+          </div>
+
+          <div style={{ position: 'absolute', left: '-105px', bottom: '-20px', top: '300px' }}>
+            <p className="text-sm text-custom-black font-notosansjp mt-2 mb-10">
+              {showGasInfo && "Viewing Gas."}
+              {showWeatherInfo && "Viewing Weather."}
+              {!showGasInfo && !showWeatherInfo && "Select an option below."}
+            </p>
+            <div className="flex"> 
+              <button
+                onClick={toggleGasInfo}
+                className={`font-notosansjp font-extrabold mr-4 text-custom-black ${showGasInfo ? 'bg-custom-green4' : 'bg-custom-green3'} py-1 px-2 rounded-md mb-2`}
+              >
+                Gas 
+              </button>
+              <button
+                onClick={toggleWeatherInfo}
+                className={`font-notosansjp font-extrabold ml-4 text-custom-black ${showWeatherInfo ? 'bg-custom-green4' : 'bg-custom-green3'} py-1 px-2 rounded-md mb-2`}
+              >
+                Weather
+              </button>
             </div>
+            {showGasInfo && <Gas showGasInfo={showGasInfo} />}
+            {showWeatherInfo && <Weather />}
           </div>
 
-          <div style={{ position: 'absolute', left: '-100px', top: '300px' }}>
-            <button
-              onClick={toggleGasInfo}
-              className={`font-notosansjp font-extrabold mr-10 text-custom-black bg-${showGasInfo ? 'custom-green4' : 'custom-green3'} hover:bg-custom-green4 py-1 px-2 rounded-md mb-2`}
-            >
-              Gas Stations
-            </button>
-            {showGasInfo && (
-              <Gas
-              />
-            )}
-
-            <button
-              onClick={toggleWeatherInfo}
-              className={`font-notosansjp font-extrabold mt-10 text-custom-black bg-${showWeatherInfo ? 'custom-green4' : 'custom-green3'} hover:bg-custom-green4 py-1 px-2 rounded-md mb-2`}
-            >
-              Weather
-            </button>
-          </div>
           <Map showGasInfo={showGasInfo} />
         </div>
       </div>
