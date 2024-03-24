4ae73e3f702a4a202b9f44e781869b4fc17c18f9
Emma Zimmerman <emma.ar.zimmerman@gmail.com>
2024-03-23 22:07:10 -0400

    refactor: updated to match requirements

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,4 +1,3 @@
-// About.js
 import React from 'react';
 
 function About() {
@@ -8,15 +7,13 @@ function About() {
         <h2 className="font-notosansjp font-bold text-center mb-12"> About TripEase</h2>
         <div className="max-w-md w-full px-8 py-6 bg-white rounded-lg shadow-md">
           <p className="text-lg leading-relaxed">
-            TripEase is a web application tailored for individuals at all levels of travel experience to streamline the process of planning any trip. The anticipated sign-up process will guide users through the journey planning process, simplifying tasks from selecting departure and destination points to finalizing itineraries within an intuitive web interface. This web application will adapt its interface to fit various screen sizes on mobile devices to ensure flexibility for users on the go.
+            TripEase is a web application tailored for individuals at all levels of travel experience to streamline the process of planning any trip. The anticipated sign-up process will guide users through the journey planning process, simplifying tasks from selecting departure and destination points to finalizing itineraries within an intuitive web interface. 
           </p>
-
           <p className="text-lg leading-relaxed">
-            TripEase will provide users with an effortless means to discover rental cars within their specified budget by integrating with car rental APIs. Users can filter cars based on price, vehicle type, and features, ensuring a customized selection for a personalized road trip adventure. Additionally, TripEase will show flights to their destinations, empowering users to make informed decisions without sacrificing convenience. For budget-conscious travelers, TripEase will locate convenient and cost-effective gas stops along their route. The application will inform users about unpredictable weather conditions through real-time weather updates.
+          For budget-conscious travelers, TripEase will locate convenient and cost-effective gas stops along their route. The application will inform users about unpredictable weather conditions through real-time weather updates.
           </p>
-
           <p className="text-lg leading-relaxed">
-            TripEase's integration with various APIs guarantees a seamless flow of current information, encompassing possible details such as weather updates, flight options, gas prices, and car rentals. This comprehensive integration will enhance the efficiency of the web application, ensuring users have accurate and up-to-date insights for a more streamlined trip-planning experience.
+          TripEase's integration with various APIs guarantees a seamless flow of current information, encompassing weather updates and gas prices. This comprehensive integration will enhance the efficiency of the web application, ensuring users have accurate and up-to-date insights for a more streamlined trip-planning experience.
           </p>
         </div>
       </div>
