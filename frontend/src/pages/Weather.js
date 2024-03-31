ef594601d82d312acbdb054a8ebde27b20ebffd3
Emma Zimmerman <emma.ar.zimmerman@gmail.com>
2024-03-30 21:23:52 -0400

    Fix: Updated the format of the Weather page

--- frontend/src/pages/Weather.js
+++ frontend/src/pages/Weather.js
@@ -7,6 +7,7 @@ const Weather = () => {
 
   const handleOptionChange = (value) => {
     setSelectedOption(value);
+    setShowLegend(false);
   };
 
   const toggleLegend = () => {
@@ -14,49 +15,53 @@ const Weather = () => {
   };
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', marginTop: '20px' }}>
-      <label>
-        <input
-          type="radio"
-          name="weatherOption"
-          value="overlay"
-          checked={selectedOption === 'overlay'}
-          onChange={() => handleOptionChange('overlay')}
-        />
-        Show Weather Overlay
-      </label>
-
-      <label style={{ marginTop: '10px' }}>
-        <input
-          type="radio"
-          name="weatherOption"
-          value="radar"
-          checked={selectedOption === 'radar'}
-          onChange={() => handleOptionChange('radar')}
-        />
-        Show Weather Radar
-      </label>
-
-      {selectedOption === 'radar' && (
-        <div style={{ display: 'flex', justifyContent: 'space-between', padding: '10px', marginTop: '-5px', marginLeft: '15px' }}>
+    <div style={{ marginTop: '20px', position: 'relative', fontFamily: 'Noto Sans JP', fontSize: '14px', display: 'flex', justifyContent: 'space-between' }}>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', }}>
+        <p style={{ marginLeft: '10px' }}> Select Weather View:</p>
+        <div style={{ display: 'flex', marginBottom: '10px', marginTop:'10px' }}>
+          <div style={{ marginLeft: '10px', marginRight: '20px' }}>
+            <input
+              type="radio"
+              id="overlay"
+              name="weatherOption"
+              value="overlay"
+              checked={selectedOption === 'overlay'}
+              onChange={() => handleOptionChange('overlay')}
+              style={{ marginRight: '5px' }}
+            />
+            <label> Weather Overlay </label>
+          </div>
           <div>
-            <p className="text-sm text-custom-black font-notosansjp">Select A Radar View: </p>
-            <select value={selectedRadar} onChange={(e) => setSelectedRadar(e.target.value)}>
-              <option value="">Radar View </option>
-            </select>
+            <input
+              type="radio"
+              id="radar"
+              name="weatherOption"
+              value="radar"
+              checked={selectedOption === 'radar'}
+              onChange={() => handleOptionChange('radar')}
+              style={{ marginRight: '5px' }}
+            />
+            <label> Weather Radar </label>
           </div>
         </div>
-      )}
 
-      <div style={{ position: 'absolute', left: '10px', bottom: '-10px', top: '350px' }}>
-        <p className="text-m text-custom-black font-notosansjp">
-          Alerts Along Your Route: 
-        </p>
+        {selectedOption === 'radar' && (
+          <div style={{ marginTop: '15px', marginBottom: '20px', marginLeft: '30px' }}>
+            <label style={{ fontWeight: 'bold', fontSize: '14px', color: '#000' }}>Select A Radar View:</label>
+            <select value={selectedRadar} onChange={(e) => setSelectedRadar(e.target.value)} style={{ marginLeft: '10px', fontSize: '14px' }}>
+              <option value="">Radar View</option>
+            </select>
+          </div>
+        )}
+
+        <div style={{ position: 'absolute', left: '10px', bottom: '-10px', top: '180px' }}>
+          <p>Alerts Along Your Route:</p>
+        </div>
       </div>
-  
-      <div style={{ marginTop: '180px', marginLeft: 'auto', marginRight: '-400px' }}>
+
+      <div style={{ marginTop: '310px', marginLeft: '220px'}}>
         <button
-          className={`font-notosansjp font-extrabold text-custom-black py-1 px-2 rounded-md mb-2 ${showLegend ? 'bg-custom-green4' : 'bg-custom-green3'} ${selectedOption ? 'bg-custom-green4' : ''}`}
+          className={`font-notosansjp font-extrabold text-custom-black py-1 px-2 rounded-md mb-2 ${showLegend ? 'bg-custom-green4' : 'bg-custom-green3'} hover:bg-custom-green4`}
           onClick={toggleLegend}
         >
           {showLegend ? 'Hide' : 'Show'} Legend
