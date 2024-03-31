992e09f95671ad449cde74a967f8c51973862db6
Lukas DeLoach <lukasdeloach@gmail.com>
2024-03-31 14:44:52 -0400

    fix: formatting fix

--- frontend/src/pages/Plan.js
+++ frontend/src/pages/Plan.js
@@ -13,19 +13,19 @@ function Plan() {
   const [polyline, setPolyline] = useState();
   const [startAddress, setStartAddress] = useState("")
   const [endAddress, setEndAddress] = useState("")
- 
+
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
   };
 
   const toggleGasInfo = () => {
     setShowGasInfo(!showGasInfo);
-    setShowWeatherInfo(false); 
+    setShowWeatherInfo(false);
   };
 
   const toggleWeatherInfo = () => {
     setShowWeatherInfo(!showWeatherInfo);
-    setShowGasInfo(false); 
+    setShowGasInfo(false);
   };
 
   const closeSidebar = () => {
@@ -34,11 +34,11 @@ function Plan() {
 
   return (
     <div className="min-h-screen bg-custom-green flex justify-center items-center font-notosansjp font-bold m-1 relative z-0">
-      <div className="w-full max-w-screen-xl relative h-full"> 
+      <div className="w-full max-w-screen-xl relative h-full">
         <button onClick={toggleSidebar} className="absolute top-2 left-[-6rem] z-0 text-xl text-white">
           <GiHamburgerMenu />
         </button>
-        
+
         <Sidebar isOpen={isSidebarOpen} onClose={closeSidebar}>
           <div style={{ padding: '20px', borderRight: '2px solid white' }}>
             <h2 className="font-notosansjp font-extrabold text-custom-black">Saved Trips</h2>
@@ -62,12 +62,12 @@ function Plan() {
               {showWeatherInfo && "Viewing Weather."}
               {!showGasInfo && !showWeatherInfo && "Select an option below."}
             </p>
-            <div className="flex"> 
+            <div className="flex">
               <button
                 onClick={toggleGasInfo}
                 className={`font-notosansjp font-extrabold mr-4 text-custom-black ${showGasInfo ? 'bg-custom-green4' : 'bg-custom-green3'} py-1 px-2 rounded-md mb-2 hover:bg-custom-green4`}
               >
-                Gas 
+                Gas
               </button>
               <button
                 onClick={toggleWeatherInfo}
@@ -76,11 +76,11 @@ function Plan() {
                 Weather
               </button>
             </div>
-            {showGasInfo && <Gas showGasInfo={showGasInfo} setSelectedGasStations={setSelectedData} getPolyline={polyline}/>}
+            {showGasInfo && <Gas showGasInfo={showGasInfo} setSelectedGasStations={setSelectedData} getPolyline={polyline} />}
             {showWeatherInfo && <Weather />}
           </div>
 
-          <Map showGasInfo={showGasInfo} data={data} setPolyline={setPolyline} setStartAddress={setStartAddress} setEndAddress={setEndAddress}/>
+          <Map showGasInfo={showGasInfo} data={data} setPolyline={setPolyline} setStartAddress={setStartAddress} setEndAddress={setEndAddress} />
         </div>
       </div>
     </div>
