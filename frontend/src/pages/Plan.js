56d73a79d03c6fe3d16ae634592425d9774a59da
Emma Zimmerman <emma.ar.zimmerman@gmail.com>
2024-04-02 20:34:27 -0400

    Fix: Adjusted Format

--- frontend/src/pages/Plan.js
+++ frontend/src/pages/Plan.js
@@ -13,19 +13,19 @@ function Plan() {
   const [polyline, setPolyline] = useState("");
   const [startAddress, setStartAddress] = useState("")
   const [endAddress, setEndAddress] = useState("")
- 
+
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
   };
 
   const toggleGasInfo = () => {
     setShowGasInfo(!showGasInfo);
-    setShowWeatherInfo(false); 
+    setShowWeatherInfo(false);
   };
 
   const toggleWeatherInfo = () => {
     setShowWeatherInfo(!showWeatherInfo);
-    setShowGasInfo(false); 
+    setShowGasInfo(false);
   };
 
   const closeSidebar = () => {
@@ -34,41 +34,41 @@ function Plan() {
 
   return (
     <div className='bg-custom-green'>
-      <div> 
-        <button onClick={toggleSidebar} className="absolute z-0 text-xl text-white">
+      <div>
+        <button onClick={toggleSidebar} className="absolute z-0 text-xl text-custom-black" style={{ marginLeft: '5px', marginTop: '20px' }}>
           <GiHamburgerMenu />
         </button>
         
         <Sidebar isOpen={isSidebarOpen} onClose={closeSidebar}>
-          <div style={{ padding: '20px', borderRight: '2px solid white' }}>
-            <h2 className="font-notosansjp font-extrabold text-custom-black">Saved Trips</h2>
+          <div style={{ padding: '20px'}}>
+            <h2 className="font-notosansjp text-custom-black font-bold">Saved Trips</h2>
           </div>
         </Sidebar>
-        <p className="mt-2 text-center text-3xl text-custom-black">
+        <p className="font-notosansjp text-custom-black font-bold mt-2 text-3xl text-center">
           Let's Start Planning Your Trip!
         </p>
-        <p className="text-sm text-custom-black font-notosansjp text-center">
+        <p className="font-notosansjp text-custom-black font-semibold text-sm text-center">
           Provide your origin and destination locations to begin.
         </p>
-  
-        <div className="flex flex-row m-2 p-2 justify-between">
+
+        <div className="font-notosansjp text-custom-black font-semibold flex flex-row m-2 p-2 justify-between">
           <Map showGasInfo={showGasInfo} data={data} setPolyline={setPolyline} setStartAddress={setStartAddress} setEndAddress={setEndAddress}/>
           <div className='flex flex-col'>
-            <p className="text-sm text-custom-black font-notosansjp">
-              {showGasInfo && <div className='text-center'>Viewing Gas</div>}
-              {showWeatherInfo && "Viewing Weather."}
+            <p className="font-notosansjp text-custom-black font-sem  text-sm ">
+              {showGasInfo && <div className='text-center'>Viewing Gas.</div>}
+              {showWeatherInfo && <div className='text-center'>Viewing Weather.</div>} 
               {!showGasInfo && !showWeatherInfo && "Select an option below."}
             </p>
             <div className='items-center'>
               <button
                 onClick={toggleGasInfo}
-                className={`font-notosansjp font-extrabold mr-4 text-custom-black ${showGasInfo ? 'bg-custom-green4' : 'bg-custom-green3'} py-1 px-2 rounded-md mb-2 hover:bg-custom-green4`}
+                className={`font-notosansjp text-custom-black font-semibold mr-4  ${showGasInfo ? 'bg-custom-green4' : 'bg-custom-green3'} py-1 px-2 rounded-md mb-2 hover:bg-custom-green4`}
               >
                 Gas 
               </button>
               <button
                 onClick={toggleWeatherInfo}
-                className={`font-notosansjp font-extrabold ml-4 text-custom-black ${showWeatherInfo ? 'bg-custom-green4' : 'bg-custom-green3'} py-1 px-2 rounded-md mb-2 hover:bg-custom-green4`}
+                className={`font-notosansjp text-custom-black font-semibold ml-4 ${showWeatherInfo ? 'bg-custom-green4' : 'bg-custom-green3'} py-1 px-2 rounded-md mb-2 hover:bg-custom-green4`}
               >
                 Weather
               </button>
@@ -77,13 +77,11 @@ function Plan() {
             {showWeatherInfo && <Weather />}
           </div>
           <div>
-            </div>
+          </div>
         </div>
       </div>
     </div>
-  );
-  
-    
+  );  
 }
 
 export default Plan;
