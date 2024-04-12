66867e7cbbbe6fe0fbd6b4a34b00da65140c1874
Amberichu <4722249+davchoo@users.noreply.github.com>
2024-04-12 18:04:16 -0400

    refactor: Add default base url and set withCredentials for axios
    No need to do string interpolation for the urls

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,12 @@
+import axios from 'axios';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+axios.defaults.baseURL = process.env.REACT_APP_API_URL
+axios.defaults.withCredentials = true
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
