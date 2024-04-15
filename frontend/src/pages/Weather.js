4e8e56bfd09425998042d467b7f5a881b6afeffe
Emma Zimmerman <emma.ar.zimmerman@gmail.com>
2024-04-15 12:32:36 -0400

    Feature: Added three legends

--- frontend/src/pages/Weather.js
+++ frontend/src/pages/Weather.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import weatherAlerts from '../pages/weather_alert.json'; // Importing the JSON for testing  
+import wxColorMap from './wxColorMap';
+import { qpfColorMap } from './qpfColorMap';
+import { tempColorMap } from './tempColorMap';
 
 const Weather = () => {
   const [selectedOption, setSelectedOption] = useState(null);
@@ -26,6 +29,12 @@ const Weather = () => {
 
   const toggleExpand = (alert) => {
     setExpandedAlert(expandedAlert === alert ? null : alert);
+  };
+
+  const qpfRange = (index) => {
+    const min = index * 0.05;
+    const max = (index + 1) * 0.05;
+    return `${min}in - ${max}in`;
   };
 
   return (
@@ -58,79 +67,133 @@ const Weather = () => {
             <label>Weather Radar</label>
           </div>
         </div>
-
-        {selectedOption === 'radar' && (
-          <div className="mt-4 ml-4">
-            <label className="font-notosansjp text-custom-black font-semibold text-md">Select A Radar View:</label>
-            <select
-              value={selectedRadar}
-              onChange={(e) => setSelectedRadar(e.target.value)}
-              className="ml-2 text-md"
-            >
-              <option value="">Radar View</option>
-            </select>
-          </div>
-        )}
       </div>
+
+      {selectedOption === 'radar' && (
+        <div className="mt-4">
+          <label htmlFor="radarSelect" className="font-notosansjp text-custom-black font-semibold">Select Radar:</label>
+          <select
+            id="radarSelect"
+            className="ml-2"
+            value={selectedRadar}
+            onChange={(e) => setSelectedRadar(e.target.value)}
+          >
+            <option value="">Choose A Radar</option>
+          </select>
+        </div>
+      )}
+
+      {selectedOption && (
+        <div>
+          <button
+            className={`font-notosansjp text-custom-black font-semibold py-1 px-2 rounded-md mb-2 mt-20 ${
+              showLegend ? 'bg-custom-green4' : 'bg-custom-green3'
+            } hover:bg-custom-green4`}
+            onClick={toggleLegend}
+          >
+            {showLegend ? 'Hide' : 'Show'} Legend
+          </button>
+
+          {showLegend && selectedOption === 'overlay' && (
+            <div className="mt-4" style={{ width: '300px' }}>
+              <p className="font-notosansjp text-custom-black">Weather Overlay Legend:</p>
+              <div className="mt-4 flex flex-wrap">
+                {Object.entries(wxColorMap).map(([key, color]) => (
+                  <div key={key} className="flex items-center mr-2 mb-2" style={{ width: '80px' }}>
+                    <div className="w-4 h-4" style={{ backgroundColor: color, border: '1px solid black' }}></div>
+                    <p className="font-notosansjp text-custom-black ml-2 text-xs">{key}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
+
+          {showLegend && selectedOption === 'radar' && (
+            <div>
+              <div className="mt-4" style={{ width: '300px' }}>
+                <p className="font-notosansjp text-custom-black">Quantitative Precipitation Forecast (QPF) Legend (inches):</p>
+                <div className="mt-4 flex items-center">
+                  <div className="w-full flex justify-between">
+                    <div className="w-1/6 font-notosansjp text-custom-black">0.00</div>
+                    <div className="w-4/6 h-6 flex justify-between">
+                      {qpfColorMap.slice(1, -1).map((color, index) => (
+                        <div key={index} className="w-1/6 h-full" style={{ backgroundColor: color }}></div>
+                      ))}
+                    </div>
+                    <div className="w-1/12 font-notosansjp text-custom-black text-right">10.00</div>
+                  </div>
+                </div>
+                <div className="mt-2 flex justify-between">
+                  <div className="w-1/6"></div>
+                  <p className="font-notosansjp text-custom-black">0.25</p>
+                  <p className="font-notosansjp text-custom-black">2.50</p>
+                  <p className="font-notosansjp text-custom-black">5.00</p>
+                  <p className="font-notosansjp text-custom-black">7.50</p>
+                  <div className="w-1/6"></div>
+                </div>
+              </div>
+
+              <div className="mt-4" style={{ width: '300px' }}>
+                <p className="font-notosansjp text-custom-black">Temperature Legend (°F):</p>
+                <div className="mt-4 flex items-center">
+                  <div className="w-full flex justify-between">
+                    <div className="w-1/6 font-notosansjp text-custom-black">-10.0</div>
+                    <div className="w-4/6 h-6 flex justify-between">
+                      {tempColorMap.map((color, index) => (
+                        <div key={index} className="w-1/6 h-full" style={{ backgroundColor: color }}></div>
+                      ))}
+                    </div>
+                    <div className="w-1/12 font-notosansjp text-custom-black text-right">110.0</div>
+                  </div>
+                </div>
+                <div className="mt-2 flex justify-between">
+                  <div className="w-1/6"></div>
+                  <p className="font-notosansjp text-custom-black">-5.0</p>
+                  <p className="font-notosansjp text-custom-black">0.0</p>
+                  <p className="font-notosansjp text-custom-black">50.0</p>
+                  <p className="font-notosansjp text-custom-black">100.0</p>
+                  <div className="w-1/6"></div>
+                </div>
+              </div>
+            </div>
+          )}
+
+        </div>
+      )}
 
       <div className="mt-20">
         <p className="font-notosansjp text-custom-black font-semibold">Alerts Along Your Route:</p>
         <div className="alert-container" style={{ maxHeight: '80vh', width: '300px', overflowY: 'auto' }}>
-          {Object.entries(sortedAlerts.reduce((acc, alert) => {
-            if (!acc[alert.event]) {
-              acc[alert.event] = [];
-            }
-            acc[alert.event].push(alert);
-            return acc;
-          }, {})).map(([event, alertsForEvent], index) => (
+          {sortedAlerts.map((alert, index) => (
             <div key={index}>
-              <p className="font-notosansjp text-custom-black font-semibold underline">{event}:</p>
+              <p className="font-notosansjp text-custom-black font-semibold underline">{alert.event}:</p>
               <ul className="list-disc pl-8">
-                {alertsForEvent.map((alert, idx) => (
-                  <li key={idx} className="mt-2">
-                    <p className="font-notosansjp text-custom-black font-semibold">
-                      Sent: {new Date(alert.sent).toLocaleString()}
-                    </p>
-                    <p className="font-notosansjp text-custom-black font-semibold">
-                      Expires: {new Date(alert.expires).toLocaleString()}
-                    </p>
+                <li key={index} className="mt-2">
+                  <p className="font-notosansjp text-custom-black font-semibold">
+                    Sent: {new Date(alert.sent).toLocaleString()}
+                  </p>
+                  <p className="font-notosansjp text-custom-black font-semibold">
+                    Expires: {new Date(alert.expires).toLocaleString()}
+                  </p>
 
-                    <p className="font-notosansjp text-custom-black font-semibold">Status: {alert.status}</p>
-                    <p className="font-notosansjp text-custom-black">Area Description: {alert.areaDescription}</p>
-                    <p className="font-notosansjp text-custom-black">Severity: {alert.severity}</p>
-                    <p className="font-notosansjp text-custom-black">Headline: {alert.headline}</p>
-                    {expandedAlert === alert && (
-                      <div>
-                        <p className="font-notosansjp text-custom-black">Description: {alert.description}</p>
-                        <p className="font-notosansjp text-custom-black">Instruction: {alert.instruction}</p>
-                      </div>
-                    )}
-                    <button onClick={() => toggleExpand(alert)}>
-                      {expandedAlert === alert ? 'Collapse Description and Instructions' : 'Expand Description and Instructions'}
-                    </button>
-                  </li>
-                ))}
+                  <p className="font-notosansjp text-custom-black font-semibold">Status: {alert.status}</p>
+                  <p className="font-notosansjp text-custom-black">Area Description: {alert.areaDescription}</p>
+                  <p className="font-notosansjp text-custom-black">Severity: {alert.severity}</p>
+                  <p className="font-notosansjp text-custom-black">Headline: {alert.headline}</p>
+                  {expandedAlert === alert && (
+                    <div>
+                      <p className="font-notosansjp text-custom-black">Description: {alert.description}</p>
+                      <p className="font-notosansjp text-custom-black">Instruction: {alert.instruction}</p>
+                    </div>
+                  )}
+                  <button onClick={() => toggleExpand(alert)}>
+                    {expandedAlert === alert ? 'Collapse Description and Instructions' : 'Expand Description and Instructions'}
+                  </button>
+                </li>
               </ul>
             </div>
           ))}
         </div>
-      </div>
-
-      <div>
-        <button
-          className={`font-notosansjp text-custom-black font-semibold py-1 px-2 rounded-md mb-2 mt-20 ${
-            showLegend ? 'bg-custom-green4' : 'bg-custom-green3'
-          } hover:bg-custom-green4`}
-          onClick={toggleLegend}
-        >
-          {showLegend ? 'Hide' : 'Show'} Legend
-        </button>
-
-        {showLegend && (
-          <div className="mt-4">
-            <p className="font-notosansjp text-custom-black">Legend Content Here</p>
-          </div>
-        )}
       </div>
     </div>
   );
