6c8e8eb4fc15791e2aeb967e18bb631be423a610
Kat Saburova <yeksbr@gmail.com>
2024-04-25 13:43:43 -0400

    Added LinkedIns on Contact page

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -12,11 +12,11 @@ function Contact() {
 
           <div className="mt-4">
             <ul className="list-disc">
-              <li className="text-lg"> Emma Zimmerman (<a href="https://github.com/EmmaZim">GitHub</a>, <a href="mailto:emma.ar.zimmerman@gmail.com">emma.ar.zimmerman@gmail.com</a>)</li>
-              <li className="text-lg"> David Choo (<a href="https://github.com/davchoo">GitHub</a>)</li>
-              <li className="text-lg"> Lukas DeLoach (<a href="https://github.com/lukasdeloach">GitHub</a>)</li>
-              <li className="text-lg"> Kaan Kayis (<a href="https://github.com/kaankayis22">GitHub</a>)</li>
-              <li className="text-lg"> Yekaterina Saburova (<a href="https://github.com/KatSaburova">GitHub</a>, <a href="mailto:yeksbr@gmail.com">yeksbr@gmail.com</a>)</li>
+            <li className="text-lg"> Emma Zimmerman (<a href="https://github.com/EmmaZim">GitHub</a>, <a href="https://www.linkedin.com/in/emma-zimmerman-4aa317267/">LinkedIn</a>)</li>
+              <li className="text-lg"> David Choo (<a href="https://github.com/davchoo">GitHub</a>, <a href="https://www.linkedin.com/in/choo-david">LinkedIn</a>)</li>
+              <li className="text-lg"> Lukas DeLoach (<a href="https://github.com/lukasdeloach">GitHub</a>, <a href="https://www.linkedin.com/in/lukas-deloach/">LinkedIn</a>)</li>
+              <li className="text-lg"> Kaan Kayis (<a href="https://github.com/kaankayis22">GitHub</a>, <a href="https://www.linkedin.com/in/kaan-kayis-b6b708266/">LinkedIn</a>)</li>
+              <li className="text-lg"> Yekaterina Saburova (<a href="https://github.com/KatSaburova">GitHub</a>, <a href="https://www.linkedin.com/in/yekaterina-saburova-0aa257242/">LinkedIn</a>)</li>
             </ul>
           </div>
         </div>
