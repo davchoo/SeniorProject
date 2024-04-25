91639296441fc49b25082e54e6060398a07f791a
Amberichu <4722249+davchoo@users.noreply.github.com>
2024-04-25 13:30:29 -0400

    fix: Expand legend to full width of sidebar
    Add Weather radar button
    Make time slider in forecasted route grow

--- frontend/src/pages/Weather.js
+++ frontend/src/pages/Weather.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect, useMemo } from 'react';
-import wxColorMap from './wxColorMap';
-import { qpfColorMap } from './qpfColorMap';
-import { tempColorMap } from './tempColorMap';
 import axios from "axios"
+import { PrecipitationLegend, TemperatureLegend, WXLegend } from '../components/Legends';
 
 const RouteStartSlider = ({ setRouteStartTime }) => {
   const [selectedDateTime, setSelectedDateTime] = useState(0);
@@ -38,6 +36,7 @@ const RouteStartSlider = ({ setRouteStartTime }) => {
           max={dateTimes.length - 1}
           value={selectedDateTime}
           onChange={(e) => handleDateTimeChange(parseInt(e.target.value))}
+          className='flex-grow'
         />
         <button
           onClick={handleConfirmDateClick}
@@ -188,26 +187,19 @@ const Weather = ({ setForecastedRoute, weatherAlerts, setRouteStartTime, availab
         <div className="mt-8">
           <p className="font-notosansjp text-custom-black font-semibold">Select Radar:</p>
           <div className="flex">
-            <button
-              className={`mr-2 py-1 px-2 rounded-md font-notosansjp text-custom-black font-semibold ${selectedRadar === 'temperature' ? 'bg-custom-green4' : 'bg-custom-green3'} hover:bg-custom-green4`}
-              onClick={() => {
-                setSelectedRadar('temperature');
-                setShowLegend(true);
-                setShowLegendButton(true);
-              }}
-            >
-              Temperature
-            </button>
-            <button
-              className={`py-1 px-2 rounded-md font-notosansjp text-custom-black font-semibold ${selectedRadar === 'precipitation' ? 'bg-custom-green4' : 'bg-custom-green3'} hover:bg-custom-green4`}
-              onClick={() => {
-                setSelectedRadar('precipitation');
-                setShowLegend(true);
-                setShowLegendButton(true);
-              }}
-            >
-              Precipitation
-            </button>
+            {['weather', 'temperature', 'precipitation'].map((value) => (
+              <button
+                key={value}
+                className={`mr-2 py-1 px-2 rounded-md font-notosansjp text-custom-black font-semibold ${selectedRadar === value ? 'bg-custom-green4' : 'bg-custom-green3'} hover:bg-custom-green4`}
+                onClick={() => {
+                  setSelectedRadar(value);
+                  setShowLegend(true);
+                  setShowLegendButton(true);
+                }}
+              >
+                {value.substring(0, 1).toUpperCase() + value.substring(1)}
+              </button>
+            ))}
           </div>
         </div>
       )}
@@ -223,69 +215,10 @@ const Weather = ({ setForecastedRoute, weatherAlerts, setRouteStartTime, availab
             {showLegend ? 'Hide' : 'Show'} Legend
           </button>
 
-          {showLegend && selectedOption === 'forecasted-route' && (
-            <div className="mt-4" style={{ width: '300px' }}>
-              <p className="font-notosansjp text-custom-black">Weather Forecast Legend:</p>
-              <div className="mt-4 flex flex-wrap">
-                {Object.entries(wxColorMap).map(([key, color]) => (
-                  <div key={key} className="flex items-center mr-2 mb-2" style={{ width: '80px' }}>
-                    <div className="w-4 h-4" style={{ backgroundColor: color, border: '1px solid black' }}></div>
-                    <p className="font-notosansjp text-custom-black ml-2 text-xs">{key}</p>
-                  </div>
-                ))}
-              </div>
-            </div>
-          )}
-
-          {showLegend && selectedRadar === 'precipitation' && selectedOption === 'radar' && (
-            <div className="mt-4" style={{ width: '300px' }}>
-              <p className="font-notosansjp text-custom-black">Quantitative Precipitation Forecast (QPF) Legend (inches):</p>
-              <div className="mt-4 flex items-center">
-                <div className="w-full flex justify-between">
-                  <div className="w-1/6 font-notosansjp text-custom-black">0.00</div>
-                  <div className="w-4/6 h-6 flex justify-between">
-                    {qpfColorMap.slice(1, -1).map((color, index) => (
-                      <div key={index} className="w-1/6 h-full" style={{ backgroundColor: color }}></div>
-                    ))}
-                  </div>
-                  <div className="w-1/12 font-notosansjp text-custom-black text-right">10.00</div>
-                </div>
-              </div>
-              <div className="mt-2 flex justify-between">
-                <div className="w-1/6"></div>
-                <p className="font-notosansjp text-custom-black">0.25</p>
-                <p className="font-notosansjp text-custom-black">2.50</p>
-                <p className="font-notosansjp text-custom-black">5.00</p>
-                <p className="font-notosansjp text-custom-black">7.50</p>
-                <div className="w-1/6"></div>
-              </div>
-            </div>
-          )}
-
-          {showLegend && selectedRadar === 'temperature' && selectedOption === 'radar' && (
-            <div className="mt-4" style={{ width: '300px' }}>
-              <p className="font-notosansjp text-custom-black">Temperature Legend (°F):</p>
-              <div className="mt-4 flex items-center">
-                <div className="w-full flex justify-between">
-                  <div className="w-1/6 font-notosansjp text-custom-black">-10.0</div>
-                  <div className="w-4/6 h-6 flex justify-between">
-                    {tempColorMap.map((color, index) => (
-                      <div key={index} className="w-1/6 h-full" style={{ backgroundColor: color }}></div>
-                    ))}
-                  </div>
-                  <div className="w-1/12 font-notosansjp text-custom-black text-right">110.0</div>
-                </div>
-              </div>
-              <div className="mt-2 flex justify-between">
-                <div className="w-1/6"></div>
-                <p className="font-notosansjp text-custom-black">-5.0</p>
-                <p className="font-notosansjp text-custom-black">0.0</p>
-                <p className="font-notosansjp text-custom-black">50.0</p>
-                <p className="font-notosansjp text-custom-black">100.0</p>
-                <div className="w-1/6"></div>
-              </div>
-            </div>
-          )}
+          {showLegend && selectedOption === 'forecasted-route' && <WXLegend /> }
+          {showLegend && selectedOption === 'radar' && selectedRadar === 'weather' && <WXLegend /> }
+          {showLegend && selectedOption === 'radar' && selectedRadar === 'precipitation' && <PrecipitationLegend /> }
+          {showLegend && selectedOption === 'radar' && selectedRadar === 'temperature' && <TemperatureLegend /> }
 
         </div>
       )}
